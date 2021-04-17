671a2f7065c9e214c6a418867c611ea877dcb3d6
quartciphers <80313406+quartciphers@users.noreply.github.com>
2021-04-17 07:15:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello MoneyPechuTeam</h1>
+      <h2>Start Coding to see some magic happen!</h2>
+    </div>
+  );
+}
